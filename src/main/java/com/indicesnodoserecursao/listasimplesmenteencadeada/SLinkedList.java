536791d2f8016460c753263995af3fcdc56a906b
Pablo Zalem <zalem.pablo@yahoo.com>
2026-05-13 20:06:10 -0300

    Insercao de elementos

--- src/main/java/com/indicesnodoserecursao/listasimplesmenteencadeada/SLinkedList.java
+++ src/main/java/com/indicesnodoserecursao/listasimplesmenteencadeada/SLinkedList.java
@@ -2,10 +2,31 @@ package com.indicesnodoserecursao.listasimplesmenteencadeada;
 
 public class SLinkedList {
     protected Node head;
+    protected Node tail;
     protected long size;
 
     public SLinkedList() {
         head = null;
+        tail = null;
         size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    // Inserion First
+    public void insertFirst(Node value){
+        value.setNext(head);
+        head = value;
+        size++;
+    }
+
+    // Insertion Last
+    public void insertionLast(Node value) {
+        value.setNext(null);
+        tail.setNext(value);
+        tail = value;
+        size++;
     }
 }
